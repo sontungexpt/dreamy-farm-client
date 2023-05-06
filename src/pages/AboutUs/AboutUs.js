03029d4517e43908e64fd41f9df7a0aee07870f7
PhamNguyetQuynh <phamnguyetquynh0307@gmail.com>
2023-05-06 21:11:12 +0700

    added_about_us

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -1,5 +1,16 @@
 import styles from './AboutUs.module.scss';
+import {Hero, WhatSetsUsApart, WhyDreamyFarm} from './Hero.js';
+
 function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <>
+     <Hero 
+      cName="hero"
+      heroImg={require('../../assets/img/AboutUs.jpg')}
+     />
+     <WhyDreamyFarm />
+     <WhatSetsUsApart />
+    </>
+  );
 }
 export default AboutUs;
