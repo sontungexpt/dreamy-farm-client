be95f56963cb04e652d80dd5509258fe9e772351
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-05-09 20:53:13 +0700

    update routes logic at app.js

--- src/App.js
+++ src/App.js
@@ -6,38 +6,39 @@ import { DefaultLayout } from '~/layouts';
 import ProtectedRoute from '~/components/ProtectedRoute';
 
 function App() {
-  function handleAllowedRoutes() {}
-  function handleRoute(route, index, notProtected) {
-    let Layout = DefaultLayout;
-    if (route.layout) {
-      Layout = route.layout;
-    } else if (route.layout === null) {
-      Layout = Fragment;
-    }
+  function handleRoutes(routes, isProtected = false) {
+    return routes.map((route, index) => {
+      let Layout = DefaultLayout;
+      if (route.layout) {
+        Layout = route.layout;
+      } else if (route.layout === null) {
+        Layout = Fragment;
+      }
 
-    const Page = route.element;
+      const Page = route.element;
 
-    return (
-      <Route
-        key={index}
-        path={route.path}
-        element={
-          <Layout>
-            <ProtectedRoute isAllowed={notProtected || handleAllowedRoutes()}>
-              <Page />
-            </ProtectedRoute>
-          </Layout>
-        }
-      />
-    );
+      return (
+        <Route
+          key={index}
+          path={route.path}
+          element={
+            <Layout>
+              <ProtectedRoute isAllowed={!isProtected}>
+                <Page />
+              </ProtectedRoute>
+            </Layout>
+          }
+        />
+      );
+    });
   }
 
   return (
     <Router>
       <div className="App">
         <Routes>
-          {publicRoutes.map((route, index) => handleRoute(route, index, true))}
-          {privateRoutes.map((route, index) => handleRoute(route, index))}
+          {handleRoutes(publicRoutes)}
+          {handleRoutes(privateRoutes, true)}
         </Routes>
       </div>
     </Router>
