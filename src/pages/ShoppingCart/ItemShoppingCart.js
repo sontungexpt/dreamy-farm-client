b312491c5104d06bd06bc2495f8654f3e43b7912
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-05-16 21:48:51 +0700

    dummycounter>:

--- src/pages/ShoppingCart/ItemShoppingCart.js
+++ src/pages/ShoppingCart/ItemShoppingCart.js
@@ -2,60 +2,41 @@ import styles from './ItemShoppingCart.module.scss';
 import Image from '~/components/Image';
 import Counter from '~/components/Counter';
 import { useState, useRef } from 'react';
-import {Tag as PriceTag} from '~/assets/images/icons/SvgIcons';
+import { Tag as PriceTag } from '~/assets/images/icons/SvgIcons';
 import ToggleIcon from '~/components/ToggleIcon';
 import ModalButton from '~/components/ModalButton';
 import Button from '~/components/Button';
 import Detail from '~/pages/Products/Item/Detail';
 
 function ItemShoppingCart({
-    name,
-    price,
-    image,
-    priceTag,
-    quantity,
-    onRemove,
-  }) {
-    function handleRemove(event) {
-        event.stopPropagation();
-        onRemove && onRemove();
-      }
-      const counterRef = useRef();
-      return (
-        <div className='itemShoppingCart'>
-          <Image className={styles.image} src={image} alt="item" />
-          <div className='itemShppingCartInfo'>
-            <h3 className='itemShoppingCartName'>{name}</h3>
-            <Counter ref={counterRef} className={styles.quantity} />
-            <PriceTag className={styles.image} src={priceTag} alt='priceTag'/>
-            <h4 className='price'>{price}đ</h4>
-          </div>
-        </div>
-        // <ModalButton
-        //   closeBtn
-        //   button={
-        //     <div to="/" className={styles.wrapper}>
-        //       <Image className={styles.image} src={image} alt="item" />
-        //       <div className={styles.content}>
-        //         <div>
-        //           <h3 className={styles.name}>{name}</h3>
-        //         </div>
-        //         <div>
-        //           <p className={styles.quantity}>{quantity ? quantity : '100 g'}</p>
-        //           <p className={styles.price}>{price} đ</p>
-        //         </div>
-        //         <Button onClick={handleAdd} className={styles.addBtn} primary>
-        //           Add
-        //         </Button>
-        //       </div>
-        //     </div>
-        //   }
-        //   innerModal={
-        //     <div className={styles.innerModal}>
-        //       <Detail name={name} price={price} />
-        //     </div>
-        //   }
-        // ></ModalButton>
-      );
+  name,
+  price,
+  image,
+  priceTag,
+  quantity,
+  onRemove,
+}) {
+  function handleRemove(event) {
+    event.stopPropagation();
+    onRemove && onRemove();
   }
-  export default ItemShoppingCart;
+  const counterRef = useRef();
+  return (
+    <div className={styles.itemShoppingCart}>
+      <Image className={styles.image} src={image} alt="item" />
+      <div className={styles.itemShoppingCartInfo}>
+        <h3 className={styles.itemShoppingCartName}>{name}</h3>
+        <Counter ref={counterRef} className={styles.quantity} />
+        <div className={styles.tagPriceWrapper}>
+          <PriceTag className={styles.imageTag} src={priceTag} alt="priceTag" />
+          <h4 className={styles.price}>{price}đ</h4>
+        </div>
+
+        <div>
+          <button className="remove">Remove</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ItemShoppingCart;
