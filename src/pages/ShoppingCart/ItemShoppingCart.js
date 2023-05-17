59835efc2fb0cd2f06e02b9d01b26abe7a78e5f1
Phạm Nguyệt Quỳnh <92394926+PhamNguyetQuynh@users.noreply.github.com>
2023-05-17 22:43:33 +0700

    Shopping cart(#12)
    * begin
    
    * trying
    
    * loading1%:<
    
    * dummycounter>:
    
    * somehowgot20%loading
    
    * loading50%
    
    * loading55%
    
    * shoppingcartdone
    
    ---------
    
    Co-authored-by: PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>

---
+++ src/pages/ShoppingCart/ItemShoppingCart.js
@@ -0,0 +1,42 @@
+import styles from './ItemShoppingCart.module.scss';
+import Image from '~/components/Image';
+import Counter from '~/components/Counter';
+import { useState, useRef } from 'react';
+import { Tag as PriceTag } from '~/assets/images/icons/SvgIcons';
+import ToggleIcon from '~/components/ToggleIcon';
+import ModalButton from '~/components/ModalButton';
+import Button from '~/components/Button';
+import Detail from '~/pages/Products/Item/Detail';
+
+function ItemShoppingCart({
+  name,
+  price,
+  image,
+  priceTag,
+  quantity,
+  onRemove,
+}) {
+  const handleRemove = (event) => {
+    event.stopPropagation();
+    onRemove && onRemove();
+  };
+  const counterRef = useRef();
+  return (
+    <div className={styles.itemShoppingCart}>
+      <Image className={styles.image} src={image} alt="item" />
+      <div className={styles.itemShoppingCartInfo}>
+        <h3 className={styles.itemShoppingCartName}>{name}</h3>
+        <Counter ref={counterRef} className={styles.quantity} />
+        <div className={styles.tagPriceWrapper}>
+          <PriceTag className={styles.imageTag} src={priceTag} alt="priceTag" />
+          <h4 className={styles.price}>{price}đ</h4>
+        </div>
+
+        <button className={styles.remove} onClick={handleRemove}>
+          Remove
+        </button>
+      </div>
+    </div>
+  );
+}
+export default ItemShoppingCart;
--- src/pages/ShoppingCart/ShoppingCart.js
+++ src/pages/ShoppingCart/ShoppingCart.js
@@ -1,4 +1,85 @@
+import ItemShoppingCart from './ItemShoppingCart.js';
+import { useMemo, useEffect, useState } from 'react';
+import { productsPageConfigs as configs } from '~/configs/pages';
+import { clsx } from 'clsx';
+import styles from './ShoppingCart.module.scss';
+
 function ShoppingCart() {
-  return <h1>ShoppingCart</h1>;
+  const [pageOffset, setPageOffset] = useState(0);
+  const [products, setProducts] = useState([
+    { name: 'Product 1', price: 100 },
+    { name: 'Product 2', price: 200 },
+    { name: 'Product 3', price: 300 },
+    { name: 'Product 4', price: 400 },
+    { name: 'Product 5', price: 100 },
+    { name: 'Product 6', price: 100 },
+    { name: 'Product 7', price: 100 },
+    { name: 'Product 8', price: 100 },
+    { name: 'Product 9', price: 100 },
+  ]);
+
+  const itemsPerPage = 2;
+
+  const displayProducts = useMemo(() => {
+    const itemsVisited = pageOffset * itemsPerPage;
+    return products.slice(0, itemsVisited + itemsPerPage);
+  }, [products, pageOffset]);
+
+  const handleLoadMore = () => {
+    setPageOffset((prevOffset) => prevOffset + 1);
+  };
+
+  const handleRemoveProduct = (index) => {
+    setProducts((prevProducts) => {
+      const updatedProducts = [...prevProducts];
+      updatedProducts.splice(index, 1);
+      return updatedProducts;
+    });
+  };
+
+  const calculateTotalPrice = () => {
+    const totalPrice = products.reduce(
+      (accumulator, product) => accumulator + parseFloat(product.price),
+      0,
+    );
+    return totalPrice;
+  };
+
+  return (
+    <div className={styles.main}>
+      <h2 className={styles.title}>Shopping Cart</h2>
+      <h3
+        className={styles.subtitle}
+      >{`${products.length} products in cart`}</h3>
+
+      <div className={styles.wrapper}>
+        <div className={styles.leftWrapper}>
+          {displayProducts.map((product, index) => (
+            <ItemShoppingCart
+              key={index}
+              price={product.price}
+              name={product.name}
+              onRemove={() => handleRemoveProduct(index)}
+            />
+          ))}
+          {displayProducts.length < products.length && (
+            <button className={styles.loadMoreBtn} onClick={handleLoadMore}>
+              Load More
+            </button>
+          )}
+        </div>
+        <div className={styles.rightWrapper}>
+          <div className={styles.total}>
+            <h2>Total</h2>
+          </div>
+          <div className={styles.totalPrice}>
+            <h1>{calculateTotalPrice()}đ</h1>
+          </div>
+          <button className={styles.checkoutBtn}>Checkout</button>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default ShoppingCart;
