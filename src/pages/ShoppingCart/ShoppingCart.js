79a0b36b2a474664b00e884bff7bfc431c0a3732
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-05-17 22:08:43 +0700

    shoppingcartdone

--- src/pages/ShoppingCart/ShoppingCart.js
+++ src/pages/ShoppingCart/ShoppingCart.js
@@ -7,14 +7,15 @@ import styles from './ShoppingCart.module.scss';
 function ShoppingCart() {
   const [pageOffset, setPageOffset] = useState(0);
   const [products, setProducts] = useState([
-    'Product 1',
-    'Product 2',
-    'Product 3',
-    'Product 4',
-    'Product 5',
-    'Product 6',
-    'Product 7',
-    'Product 8',
+    { name: 'Product 1', price: 100 },
+    { name: 'Product 2', price: 200 },
+    { name: 'Product 3', price: 300 },
+    { name: 'Product 4', price: 400 },
+    { name: 'Product 5', price: 100 },
+    { name: 'Product 6', price: 100 },
+    { name: 'Product 7', price: 100 },
+    { name: 'Product 8', price: 100 },
+    { name: 'Product 9', price: 100 },
   ]);
 
   const itemsPerPage = 2;
@@ -36,38 +37,48 @@ function ShoppingCart() {
     });
   };
 
-  return (
-    // <div className={styles.container}>
-      <div className={styles.main}>
-        <h2 className={styles.title}>Shopping Cart</h2>
-        <h3
-          className={styles.subtitle}
-        >{`${products.length} products in cart`}</h3>
+  const calculateTotalPrice = () => {
+    const totalPrice = products.reduce(
+      (accumulator, product) => accumulator + parseFloat(product.price),
+      0,
+    );
+    return totalPrice;
+  };
 
-        <div className={styles.wrapper}>
-          <div className={styles.leftWrapper}>
-            {displayProducts.map((product, index) => (
-              <ItemShoppingCart
-                key={index}
-                price="100"
-                name={product}
-                onRemove={() => handleRemoveProduct(index)}
-              />
-            ))}
-            {displayProducts.length < products.length && (
-              <button className={styles.loadMoreBtn} onClick={handleLoadMore}>
-                Load More
-              </button>
-            )}
-          </div>
-          <div className={styles.rightWrapper}>
+  return (
+    <div className={styles.main}>
+      <h2 className={styles.title}>Shopping Cart</h2>
+      <h3
+        className={styles.subtitle}
+      >{`${products.length} products in cart`}</h3>
+
+      <div className={styles.wrapper}>
+        <div className={styles.leftWrapper}>
+          {displayProducts.map((product, index) => (
+            <ItemShoppingCart
+              key={index}
+              price={product.price}
+              name={product.name}
+              onRemove={() => handleRemoveProduct(index)}
+            />
+          ))}
+          {displayProducts.length < products.length && (
+            <button className={styles.loadMoreBtn} onClick={handleLoadMore}>
+              Load More
+            </button>
+          )}
+        </div>
+        <div className={styles.rightWrapper}>
+          <div className={styles.total}>
             <h2>Total</h2>
-            <h1>000đ</h1>
-            <button className={styles.checkoutBtn}>Checkout</button>
           </div>
+          <div className={styles.totalPrice}>
+            <h1>{calculateTotalPrice()}đ</h1>
+          </div>
+          <button className={styles.checkoutBtn}>Checkout</button>
         </div>
       </div>
-    // </div>
+    </div>
   );
 }
 
