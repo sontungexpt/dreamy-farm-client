eb6dfe7f03e9f2d6ca6677d0696f386b3f293ede
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-05-24 10:11:40 +0700

    add radio button

--- src/pages/Checkout/BillingAddress/BillingAddress.js
+++ src/pages/Checkout/BillingAddress/BillingAddress.js
@@ -1,18 +1,36 @@
 import styles from './BillingAddress.module.scss';
 import { clsx } from 'clsx';
 
+import React, { useState } from 'react';
+
 function BillingAddress({ name, phone, address }) {
+  const [selected, setSelected] = useState(false);
+
+  const handleSelect = () => {
+    setSelected(!selected);
+  };
+
   return (
-    <button className={clsx(['grid', styles.addressCard])}>
-      <div className={styles.info}>
-        <div className={styles.name}>
-          <p>{name}</p>
+    <button
+      className={clsx([
+        'grid',
+        styles.addressCard,
+        selected && styles.selected,
+      ])}
+    >
+      <div className={styles.addressCardLeft}>
+        <div className={styles.radioButton} onClick={handleSelect}>
+          <input type="radio" checked={selected} readOnly />
         </div>
-        <div className={styles.phone}>
-          <p>{phone}</p>
-        </div>
-        <div className={styles.address}>
-          <p>{address}</p>
+        <div className={styles.info}>
+          <div className={styles.namePhone}>
+            <p>{name}</p>
+            <span>-</span>
+            <p>{phone}</p>
+          </div>
+          <div className={styles.address}>
+            <p>{address}</p>
+          </div>
         </div>
       </div>
     </button>
