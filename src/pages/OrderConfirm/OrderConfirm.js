8634bbb2856ffa08c9c985a58c90507cd5fc91b3
PhamNguyetQuynh <phamnguyetquynh0307@yourdomain.com>
2023-05-25 20:05:53 +0700

    styling confirm page

--- src/pages/OrderConfirm/OrderConfirm.js
+++ src/pages/OrderConfirm/OrderConfirm.js
@@ -1,7 +1,18 @@
+import styles from './OrderConfirm.module.scss';
+import { GreenTick } from '~/assets/images/icons/SvgIcons';
+import { clsx } from 'clsx';
 function OrderConfirm() {
   return (
-    <div>
-      <h1>order Confirm</h1>
+    <div className={clsx([styles.body])}>
+      <div className={clsx([styles.greenTick])}>
+        <GreenTick />
+      </div>
+      <div className={clsx([styles.sub])}>
+        <h1>Order Successfully</h1>
+      </div>
+      <div className={clsx([styles.text])}>
+        <p>Please check your email for more information</p>
+      </div>
     </div>
   );
 }
