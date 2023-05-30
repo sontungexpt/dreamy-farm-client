27d3d78ccd85cb335bdc6f4844df567de8ffaa3c
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-05-30 22:15:18 +0700

    add validation login

--- src/pages/Accounts/Register/Register.js
+++ src/pages/Accounts/Register/Register.js
@@ -1,51 +1,148 @@
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
+
+import { routes as routesConfig } from '~/configs';
 import styles from './Register.module.scss';
 import inputStyles from '~/pages/Accounts/styles/InputStyles.module.scss';
-import { Link } from 'react-router-dom';
-import { routes as routesConfig } from '~/configs';
+
 import Button from '~/components/Button/Button';
 import Input from '~/components/Input';
+import validator from '~/pages/Accounts/validator';
 
 function Register() {
+  const [account, setAccount] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    errors: {},
+  });
+
+  const { t } = useTranslation('translations');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const errors = validator.validate(account);
+    if (!errors) {
+      console.log('Submit');
+    } else {
+      setAccount((prevState) => ({
+        ...prevState,
+        errors: {
+          ...prevState.errors,
+          ...errors,
+        },
+      }));
+    }
+  };
+
+  console.log(account);
+  const handleFocus = (e) => {
+    const { name } = e.target;
+
+    setAccount((prevState) => ({
+      ...prevState,
+      errors: {
+        ...prevState.errors,
+        [name]: '',
+      },
+    }));
+  };
+
+  const handleBlur = (e) => {
+    const errors = validator.validate(account);
+    setAccount((prevState) => ({
+      ...prevState,
+      errors: {
+        ...prevState.errors,
+        ...errors,
+      },
+    }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setAccount((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
   return (
     <form className={styles.wrapper}>
-      <h1 className={styles.header}>Create an account</h1>
+      <h1 className={styles.header}>{t('Create an account')}</h1>
 
       <Input
+        required
         labelClassName={inputStyles.label}
         box
-        label="Name"
+        label={t('Name')}
         id="name-register"
         type="text"
+        name="name"
+        value={account.name}
+        onChange={handleInputChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        errorMessage={account.errors.name}
       />
       <Input
+        required
         labelClassName={inputStyles.label}
         box
-        label="Email"
+        label={t('Email')}
         id="email-register"
         type="text"
+        name="email"
+        value={account.email}
+        onChange={handleInputChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        errorMessage={account.errors.email}
       />
       <Input
+        required
         labelClassName={inputStyles.label}
         box
-        label="Password"
+        label={t('Password')}
         id="password-register"
         type="password"
+        name="password"
+        value={account.password}
+        onChange={handleInputChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        errorMessage={account.errors.password}
       />
       <Input
+        required
         labelClassName={inputStyles.label}
         box
-        label="Confirm password"
+        label={t('Confirm password')}
         id="confirm-password-register"
         type="password"
+        name="confirmPassword"
+        value={account.confirmPassword}
+        onChange={handleInputChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        errorMessage={account.errors.confirmPassword}
       />
 
-      <Button primary hoverZoom className={styles.loginBtn}>
-        Create account
+      <Button
+        onClick={handleSubmit}
+        primary
+        hoverZoom
+        className={styles.loginBtn}
+      >
+        {t('Create account')}
       </Button>
       <span className={styles.loginText}>
-        Already have an account?
+        {t('Already have an account?')}
         <Link to={routesConfig.login} className={styles.loginLink}>
-          Login
+          {t('Login')}
         </Link>
       </span>
     </form>
